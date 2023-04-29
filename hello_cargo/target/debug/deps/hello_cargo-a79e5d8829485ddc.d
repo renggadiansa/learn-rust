767f10413d8906bd7951d3711771ac72404f590e
renggadiansa <renggadiansa@gmail.com>
2023-04-29 19:34:07 +0700

    cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/asus/Documents/applications/first-rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/asus/Documents/applications/first-rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/asus/Documents/applications/first-rust/hello_cargo/target/debug/hello_cargo: /home/asus/Documents/applications/first-rust/hello_cargo/src/main.rs
